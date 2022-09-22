fcc03e49a54a66b27f3e417f94e7c440a7f7bfe2
Edoardo Pieri <edoardopieri05@libero.it>
2022-09-22 17:54:46 +0200

    funzione Split e assegnazione delle proprietà nei rispettivi array

--- Form1.cs
+++ Form1.cs
@@ -7,20 +7,25 @@ namespace AS2223_3G_INF_PieriEdoardo_RubricaCSV
         {
             InitializeComponent();
         }
+        string[] nomi = new string[LENGTH];
+        string[] cognomi = new string[LENGTH];
+        string[] citta = new string[LENGTH];
         const int LENGTH = 3;
         private void btnApriFile_Click(object sender, EventArgs e)
         {
-            string[] nomi = new string[LENGTH];
-            string[] cognomi = new string[LENGTH];
-            string[] arrayDiSupporto = new string[LENGTH];
+            string[] stringheCorrenti = new string[3];
             string lineaCorrente;
             openFileDialog.ShowDialog();    //apre la selezione file
             txtNomeFile.Text = openFileDialog.FileName; //scrive nella textbox il percorso del file
             StreamReader streamreader = new StreamReader(openFileDialog.FileName);
-            while(!streamreader.EndOfStream)    //ciclo che si ripete fino a quando non finiscono le righe
+            for (int c = 0; c < LENGTH; c++)    //ciclo che si ripete fino a quando non finiscono le righe
             {
                 lineaCorrente = streamreader.ReadLine();
                 lstElenco.Items.Add(lineaCorrente);
+                stringheCorrenti = lineaCorrente.Split(';');
+                nomi[c] = stringheCorrenti[0];
+                cognomi[c] = stringheCorrenti[1];
+                citta[c] = stringheCorrenti[2];
             }
         }
 
